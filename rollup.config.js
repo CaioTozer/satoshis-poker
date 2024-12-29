45afb6d6448fa527e0ebbf985dd5dcb5a92bca5b
Caio Tozer <caiotozer@DEV-RECIFE-0774.local>
2024-12-29 00:11:48 -0300

    fix: adjusts rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,22 @@
+import babel from '@rollup/plugin-babel';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
 export default {
     input: 'src/index.js',
     output: {
         file: 'dist/bundle.js',
-        format: 'cjs',
+        format: 'esm',
+        sourcemap: true
     },
-    plugins: [],
+    plugins: [
+        babel({ babelHelpers: 'bundled' }),
+        react()
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src')
+        }
+    },
+    external: ['react', 'react-dom']
 };
